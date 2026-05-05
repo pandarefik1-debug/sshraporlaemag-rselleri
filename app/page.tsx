8052db538d75e536de826bd86763776bc7b1c174
pandarefik1-debug <pandarefik1@gmail.com>
2026-05-05 10:16:31 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
+"use client";
+
 import dynamic from "next/dynamic";
+
+// 30 bin satırlık o devasa paneli SSR'dan koruyoruz
+const Dashboard = dynamic(() => import("./Dashboard"), {
+  ssr: false,
+});
+
+export default function Page() {
+  return <Dashboard />;
+}import dynamic from "next/dynamic";
 
 // 30 bin satırlık o devasa paneli SSR'dan koruyoruz
 const Dashboard = dynamic(() => import("./Dashboard"), {
